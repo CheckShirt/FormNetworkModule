c85c61eb42f3eaab89fa29ce9922d7a78e261de2
CheckShirt <yeehaw@live.co.kr>
2020-09-09 23:32:28 +0900

    Update FormConvertible.swift

--- FormNetwork/FormConvertible.swift
+++ FormNetwork/FormConvertible.swift
@@ -12,20 +12,7 @@ import Foundation
     HTTP 통신을 사용함에 있어 Presentor에 주입할 Form 파일입니다.
   
 # Parameter
- 
-    /// 요청할 URL
-    var url: URL { get set }
-    
- 
-    /// 요청할 Method
-    var httpMethod: HTTPMethod { get }
- 
-    /// 요청할 헤더
-    var headers: [String: Any]? { get }
- 
-    /// 요청할 Body 데이터
-    var body: Data? { get }
- 
+
  */
 public protocol FormConvertible {
     
