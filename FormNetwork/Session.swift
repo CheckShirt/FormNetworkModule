8c43da9be05ea4e620ddd8ffa3c717a7b16e6c54
CheckShirt <yeehaw@live.co.kr>
2020-09-10 22:32:45 +0900

    Update Session.swift

--- FormNetwork/Session.swift
+++ FormNetwork/Session.swift
@@ -12,8 +12,27 @@ public class Session {
     
     public static let shared = Session(session: URLSession.shared)
     
-    public func requestData(_ urlRequest: URLRequest,
+    public func request(_ urlRequest: URLRequest,
                             completionHandler: @escaping (Result<Data, Error>) -> Void) {        
+        task = session.dataTask(with: urlRequest) { (data, response, error) in
+            guard let data = data else {
+                if let error = error {
+                    DispatchQueue.main.async {
+                        completionHandler(.failure(error))
+                    }
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                completionHandler(.success(data))
+            }
+        }
+        task?.resume()
+    }
+
+    public func request(_ url: URL,
+                        completionHandler: @escaping (Result<Data, Error>) -> Void) {        
+        let urlRequest = URLRequest(url: url)
         task = session.dataTask(with: urlRequest) { (data, response, error) in
             guard let data = data else {
                 if let error = error {
