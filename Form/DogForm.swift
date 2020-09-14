5913107a73e4abdb35597c13ee6f8ce96bdfa1d4
CheckShirt <yeehaw@live.co.kr>
2020-09-14 10:14:46 +0900

    first commit

--- Form/DogForm.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DogForm.swift
-//  FormNetworkExample
-//
-//  Created by HahnDante on 2020/09/08.
-//
-
-import Foundation
---
+++ FormNetworkExample/Model/LottoModel.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  LottoUI
+//
+//  Created by HahnDante on 2020/08/24.
+//  Copyright © 2020 한상민. All rights reserved.
+//
+
+import Foundation
+
+struct Lotto: Codable {
+    
+}
